4615ce0de1e28e71665b7e3e502f7aac0df4d566
ohad <ohad@apiiro.com>
2025-05-07 11:19:46 +0300

    second

--- crapi/urls.py
+++ crapi/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("api/mechanic/", include("crapi.mechanic.urls")),
+    path("api/mechanic/", include("crapi.mechanic.urls", namespace="matoki")),
+    path('path1/', admin.site.urls)
 ]
